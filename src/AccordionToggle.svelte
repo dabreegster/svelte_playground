9556b08a2a7f1ad168453f94acab185577f6422e
Dustin Carlino <dabreegster@gmail.com>
2022-12-20 18:09:42 +0000

    Refactor the header

---
+++ src/AccordionToggle.svelte
@@ -0,0 +1,11 @@
+<script>
+  import 'material-icons/iconfont/material-icons.css';
+  import IconButton, { Icon } from '@smui/icon-button';
+
+  export let open = false;
+</script>
+
+<IconButton toggle pressed={open}>
+  <Icon class="material-icons" on>expand_less</Icon>
+  <Icon class="material-icons">expand_more</Icon>
+</IconButton>
