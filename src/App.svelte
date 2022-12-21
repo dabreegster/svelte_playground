36adb49a1aac748d4775c3ef0eff14aed3fd2707
Dustin Carlino <dabreegster@gmail.com>
2022-12-21 16:01:21 +0000

    Confirming osm2streets can be used, woot

--- src/App.svelte
+++ src/App.svelte
@@ -9,15 +9,28 @@
   import Button from "@smui/button";
 
   import init, { JsStreetNetwork } from "osm2streets-js";
+  import osmInput from "./assets/input.osm?raw";
+  import boundaryGeojson from "./assets/boundary.json?raw";
 
   let panel1 = false;
   let panel2 = false;
+  let panel3 = false;
 
   let activeTab = "Residential";
 
-  init().then(() => {
-    console.log(`It worked!?`);
-  });
+  async function osm2streets() {
+    await init();
+    console.log(`Importing streets`);
+    const streets = new JsStreetNetwork(osmInput, boundaryGeojson, {
+      debug_each_step: false,
+      dual_carriageway_experiment: false,
+      cycletrack_snapping_experiment: false,
+      inferred_sidewalks: false,
+      osm2lanes: false,
+    });
+    await new Promise((r) => setTimeout(r, 5000));
+    return streets.toGeojsonPlain();
+  }
 </script>
 
 <h1>Playground</h1>
@@ -60,6 +73,19 @@
           <p>Form 3</p>
         {/if}
       </div>
+    </Content>
+  </Panel>
+  <Panel bind:open={panel3}>
+    <Header>
+      osm2streets
+      <AccordionToggle slot="icon" open="panel3" />
+    </Header>
+    <Content>
+      {#await osm2streets()}
+        <p>Loading...</p>
+      {:then geojson}
+        <pre>{geojson}</pre>
+      {/await}
     </Content>
   </Panel>
 </Accordion>
